a01fd8e932de2889db118666fa2ab99a1682d6a9
Enrique Pando <enr.pando@gmail.com>
2021-09-09 08:46:10 -0500

    Click on axie ID to go to marketplace

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,18 @@
 
     <!-- Toggle switch -->
     <div class="flex items-center mb-4">
-      <label for="toggle" class="flex items-center cursor-pointer">
+      <label
+        class="flex items-center cursor-pointer"
+        for="toggle"
+      >
         <div class="relative">
-          <input v-model="toggle" @change="toggleTimer" id="toggle" type="checkbox" class="sr-only"/>
+          <input
+            id="toggle"
+            class="sr-only"
+            type="checkbox"
+            v-model="toggle"
+            @change="toggleTimer"
+          />
           <div class="line w-14 h-8 bg-gray-300 rounded-full shadow-inner transition duration-300"></div>
           <div class="dot absolute w-6 h-6 bg-white rounded-full shadow left-1 top-1 transition duration-300"></div>
         </div>
@@ -17,11 +26,19 @@
 
     <!-- Timer -->
     <div class="mb-4">
-      <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="timer-select">
+      <label
+        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+        for="timer-select"
+      >
         Refresh rate
       </label>
       <div class="relative w-20">
-        <select v-model="timerSelect" @change="resetTimer" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="timer-select">
+        <select
+          id="timer-select"
+          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          v-model="timerSelect"
+          @change="resetTimer"
+        >
           <option value="1000">1s</option>
           <option value="2000">2s</option>
           <option value="3000">3s</option>
@@ -29,23 +46,43 @@
           <option value="5000">5s</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          <svg
+            class="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
         </div>
       </div>
     </div>
 
     <!-- Sort -->
     <div class="mb-4">
-      <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="sort-select">
+      <label
+        class="block uppercase text-gray-700 text-xs font-bold mb-2"
+        for="sort-select"
+      >
         Sort by
       </label>
       <div class="relative w-28">
-        <select v-model="sort" @change="resetTimer" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="sort-select">
+        <select
+          id="sort-select"
+          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          v-model="sort"
+          @change="resetTimer"
+        >
           <option value="newest">Newest</option>
           <option value="lowPrice">Low price</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+          <svg
+            class="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
         </div>
       </div>
     </div>
@@ -58,9 +95,21 @@
         <div class="flex-none w-2/12">ETH</div>
         <div class="flex-none w-2/12">USD</div>
       </li>
-      <li class="px-4 py-3 text-sm font-medium text-gray-500 flex" v-for="(axie, index) in axies" v-bind:key="axie.id">
+      <li
+        class="px-4 py-3 text-sm font-medium text-gray-500 flex"
+        v-for="(axie, index) in axies"
+        v-bind:key="axie.id"
+      >
         <div class="flex-none w-1/12">{{ index + 1 }}</div>
-        <div class="flex-1">#{{ axie.id }}</div>
+        <div class="flex-1">
+          <a
+            class="hover:text-green-500 transition"
+            :href="`https://marketplace.axieinfinity.com/axie/${ axie.id }`"
+            target="_blank"
+          >
+            #{{ axie.id }}
+          </a>
+        </div>
         <div class="flex-none w-2/12">{{ getETH(axie.auction.currentPrice) }}</div>
         <div class="flex-none w-2/12">${{ axie.auction.currentPriceUSD }}</div>
       </li>
@@ -82,7 +131,7 @@ export default {
     const axies = ref(null);
 
     const timer = computed(() => parseInt(timerSelect.value));
-    
+
     const startTimer = () => {
       interval.value = setInterval(updateList, timer.value);
     }
