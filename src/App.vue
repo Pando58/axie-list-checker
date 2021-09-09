22a68216ba178600f6006a6df323efbcbe6b29b8
Enrique Pando <enr.pando@gmail.com>
2021-09-08 23:18:42 -0500

    Timer working

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
     <div class="flex items-center mb-4">
       <label for="toggle" class="flex items-center cursor-pointer">
         <div class="relative">
-          <input id="toggle" type="checkbox" class="sr-only" v-model="toggle"/>
+          <input v-model="toggle" @change="toggleTimer" id="toggle" type="checkbox" class="sr-only"/>
           <div class="line w-14 h-8 bg-gray-300 rounded-full shadow-inner transition duration-300"></div>
           <div class="dot absolute w-6 h-6 bg-white rounded-full shadow left-1 top-1 transition duration-300"></div>
         </div>
@@ -21,7 +21,7 @@
         Refresh rate
       </label>
       <div class="relative w-20">
-        <select v-model="timerSelect" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="timer-select">
+        <select v-model="timerSelect" @change="resetTimer" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="timer-select">
           <option value="1000">1s</option>
           <option value="2000">2s</option>
           <option value="3000">3s</option>
@@ -40,7 +40,7 @@
         Sort by
       </label>
       <div class="relative w-28">
-        <select v-model="sort" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="sort-select">
+        <select v-model="sort" @change="resetTimer" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="sort-select">
           <option value="newest">Newest</option>
           <option value="lowPrice">Low price</option>
         </select>
@@ -78,22 +78,63 @@ export default {
     const toggle = ref(false);
     const timerSelect = ref('2000');
     const sort = ref('newest');
+    const interval = ref(null);
+    const axies = ref(null);
 
     const timer = computed(() => parseInt(timerSelect.value));
     
-    const axies = ref(null);
+    const startTimer = () => {
+      interval.value = setInterval(updateList, timer.value);
+    }
+
+    const stopTimer = () => {
+      if (interval.value !== null) {
+        clearInterval(interval.value);
+      }
+    }
+
+    const toggleTimer = () => {
+      if (toggle.value) {
+        startTimer();
+      } else {
+        stopTimer();
+      }
+    }
+
+    const resetTimer = () => {
+      if (!toggle.value) return;
+
+      stopTimer();
+      startTimer();
+    }
 
     const getETH = (eth) => {
       return (eth / Math.pow(10, 18)).toFixed(3);
     }
 
-    onMounted(() => {
+    const updateList = () => {
       getData((data) => {
         axies.value = data;
       });
+    }
+
+    onMounted(() => {
+      updateList();
     });
 
-    return { toggle, timerSelect, sort, timer, axies, getETH };
+    return {
+      toggle,
+      timerSelect,
+      sort,
+      interval,
+      axies,
+      timer,
+      startTimer,
+      stopTimer,
+      toggleTimer,
+      resetTimer,
+      getETH
+    };
   },
 }
 </script>
