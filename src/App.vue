6bcfd5575ec8e8805b8893b49d00476579ac88ef
Enrique Pando <enr.pando@gmail.com>
2021-09-10 01:37:45 -0500

    Replace sort select code with custom component

--- src/App.vue
+++ src/App.vue
@@ -16,39 +16,20 @@
         { value: 4000, name: '4s' },
         { value: 5000, name: '5s' },
       ]"
-      :selected="2000"
+      :selected="timer"
       @onChange="changeRefreshRate"
     ></Select>
 
     <!-- Sort -->
-    <div class="mb-4">
-      <label
-        class="block uppercase text-gray-700 text-xs font-bold mb-2"
-        for="sort-select"
-      >
-        Sort by
-      </label>
-      <div class="relative w-28">
-        <select
-          id="sort-select"
-          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 text-sm py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          v-model="sort"
-          @change="resetTimer"
-        >
-          <option value="newest">Newest</option>
-          <option value="lowPrice">Low price</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg
-            class="fill-current h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </div>
-      </div>
-    </div>
+    <Select 
+      label="Sort by"
+      :options="[
+        { value: 'newest', name: 'Newest' },
+        { value: 'lowPrice', name: 'Low price' }
+      ]"
+      :selected="sort"
+      @onChange="changeSort"
+    ></Select>
 
     <!-- Table -->
     <ul class="mt-12 rounded-lg shadow overflow-hidden border-b border-gray-200 divide-y divide-gray-200">
@@ -144,6 +125,11 @@ export default {
       resetTimer();
     }
 
+    const changeSort = (e, val) => {
+      sort.value = val;
+      resetTimer();
+    }
+
     onMounted(() => {
       updateList();
     });
@@ -161,7 +147,8 @@ export default {
       getETH,
       updateList,
       changeToggle,
-      changeRefreshRate
+      changeRefreshRate,
+      changeSort
     };
   },
 }
